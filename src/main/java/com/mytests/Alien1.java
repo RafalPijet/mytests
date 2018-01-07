6a572d53408336f40ad77f009ba84b22830b176c
RafalPijet <stronglopez@wp.pl>
2018-01-07 20:24:04 +0100

    drobne testy...

---
+++ src/main/java/com/mytests/Alien1.java
@@ -0,0 +1,40 @@
+package com.mytests;
+
+public class Alien1 {
+    Alien1() {
+        System.out.println("Jestem konstruktorem Alien1");
+    }
+}
+
+class Alien2 {
+    Alien2() {
+        System.out.println("Jestem konstruktorem Alien2");
+    }
+}
+
+class Alien3 {
+    Alien3() {
+        System.out.println("Jestem konstruktorem Alien3");
+    }
+}
+
+class Aliens3_1 extends Alien3 {
+    Alien1 al1 = new Alien1();
+    Alien2 al2 = new Alien2();
+    Aliens3_1() {
+        System.out.println("Jestem konstruktorem Alien3_1");
+    }
+}
+
+class Aliens3_2 extends Alien3 {
+    Aliens3_2() {
+        System.out.println("Jestem konstruktorem Alien3_2");
+    }
+
+}
+
+class Aliens_Apli {
+    public static void main(String[] args) {
+        new Aliens3_2();
+    }
+        }
