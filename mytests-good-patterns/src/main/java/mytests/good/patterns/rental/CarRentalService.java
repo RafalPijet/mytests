a83c52231c7057b40f24fa73bf956e0ca6ff7608
RafalPijet <stronglopez@wp.pl>
2018-02-18 19:02:28 +0100

    Spring wprowadzenie

--- mytests-good-patterns/src/main/java/mytests/good/patterns/rental/CarRentalService.java
+++ mytests-good-patterns/src/main/java/mytests/good/patterns/rental/CarRentalService.java
@@ -2,7 +2,7 @@ package mytests.good.patterns.rental;
 
 import java.time.LocalDateTime;
 
-public class CarRentalService implements RentalService{
+public class CarRentalService implements RentalService {
 
     public boolean rent(final User user, final LocalDateTime carRentFrom, final LocalDateTime carRentTo) {
         System.out.println("Wynajem samochodu dla: " + user.getName() + " " + user.getSurname() + " od: " + carRentFrom.toString() + " do: " + carRentTo.toString());
